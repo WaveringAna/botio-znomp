381aee60d685acd66103f6a8c6ed1d85653a2236
Aayan L <aayanzillis@gmail.com>
2017-04-23 10:49:06 -0400

    Initial commit

---
+++ on_cmd_test.js
@@ -0,0 +1,27 @@
+var botio = require(process.env['BOTIO_MODULE']);
+var shell = require('shelljs');
+
+/*echo('These are the files in your repo:');
+ls().forEach(function(file) {
+  echo('  '+file);
+});
+
+botio.message('#### Hello world');
+botio.message('Your Botio installation works! View full output for the list of files in this repo');*/
+
+shell.echo('>> Installing');
+shell.echo();
+shell.exec('npm install', {async:false});
+
+shell.echo('>> Copying bot.json');
+shell.cp(__dirname+'/z-nomp/bot.json', './pool_configs/bot.json');
+
+shell.echo('>> Copying config.json');
+shell.cp('config_example.json', 'config.json');
+
+shell.echo('>> Running z-nomp for 30 seconds');
+shell.exec('timeout 30 npm start');
+
+shell.echo('********************************************');
+shell.echo('********************************************');
+shell.echo('Done!!!!!!!');
